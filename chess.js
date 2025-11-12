363b5620246b86a4940db9132e4c2ddb0873ece2
AngeCI <145793112+AngeCI@users.noreply.github.com>
2025-11-13 04:40:39 +0800

    Basic vector image support.

--- chess.js
+++ chess.js
@@ -1,5 +1,44 @@
 "use strict";
 
+let ChessSprites = {};
+let ChessSpritesImageBitmap = {};
+const ChessSpritesMap = ["wK", "wQ", "wB", "wN", "wR", "wP", "bK", "bQ", "bB", "bN", "bR", "bP"];
+
+(async () => {
+  const parser = new DOMParser();
+  const svgDocument = parser.parseFromString(await (await fetch("img/Chess_Pieces_Sprite.svg")).text(), "image/svg+xml");
+  const allGroups = svgDocument.querySelectorAll("svg > g");
+
+  for (let i = 0; i < allGroups.length; i++) {
+    const clonedG = allGroups[i].cloneNode(true);
+    clonedG.removeAttribute("transform");
+
+    const newSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    newSvg.setAttribute("viewBox", "0 0 45 45");
+    newSvg.setAttribute("width", "100%");
+    newSvg.setAttribute("height", "100%");
+    newSvg.appendChild(clonedG);
+    ChessSprites[ChessSpritesMap[i]] = newSvg;
+  };
+
+  const spriteImg = new Image();
+  spriteImg.src = "img/Chess_Pieces_Sprite.svg";
+  spriteImg.onload = async () => {
+    ChessSpritesImageBitmap.wK = await createImageBitmap(spriteImg, 0, 0, 45, 45);
+    ChessSpritesImageBitmap.wQ = await createImageBitmap(spriteImg, 45, 0, 45, 45);
+    ChessSpritesImageBitmap.wB = await createImageBitmap(spriteImg, 90, 0, 45, 45);
+    ChessSpritesImageBitmap.wN = await createImageBitmap(spriteImg, 135, 0, 45, 45);
+    ChessSpritesImageBitmap.wR = await createImageBitmap(spriteImg, 180, 0, 45, 45);
+    ChessSpritesImageBitmap.wP = await createImageBitmap(spriteImg, 225, 0, 45, 45);
+    ChessSpritesImageBitmap.bK = await createImageBitmap(spriteImg, 0, 45, 45, 45);
+    ChessSpritesImageBitmap.bQ = await createImageBitmap(spriteImg, 45, 45, 45, 45);
+    ChessSpritesImageBitmap.bB = await createImageBitmap(spriteImg, 90, 45, 45, 45);
+    ChessSpritesImageBitmap.bN = await createImageBitmap(spriteImg, 135, 45, 45, 45);
+    ChessSpritesImageBitmap.bR = await createImageBitmap(spriteImg, 180, 45, 45, 45);
+    ChessSpritesImageBitmap.bP = await createImageBitmap(spriteImg, 225, 45, 45, 45);
+  };
+})();
+
 let Board = class {
   squares = new Uint8Array(64);
   cnv = new OffscreenCanvas(800, 800);
@@ -7,16 +46,26 @@ let Board = class {
 
   constructor() {
     this.ctx = this.cnv.getContext("2d");
-    let colours = ["#f1d9c0", "#a97a65"];
+  };
+
+  redrawCanvas(sprites = ChessSpritesImageBitmap) {
+    const colours = ["#f1d9c0", "#a97a65"];
+    const labels = [" ", "wK", "wQ", "wR", "wN", "wB", "wP", " ", " ", "bK", "bQ", "bR", "bN", "bB", "bP", " "];
+    let pieceNum;
 
     for (let y = 0; y < 8; y++) {
       for (let x = 0; x < 8; x++) {
         this.ctx.fillStyle = colours[(y ^ x) & 1];
         this.ctx.fillRect(x * 100, y * 100, 100, 100);
+
+        pieceNum = this.squares[(y << 3) + x] - 8;
+        let sprite = sprites[labels[pieceNum]];
+        if (sprite) {
+          this.ctx.drawImage(sprite, x * 100, y * 100, 100, 100);
+        };
       };
     };
   };
-
   importFromFen(fen) {
     this.squares.fill(0, 0);
     const pieceTypeTable = {
@@ -28,7 +77,7 @@ let Board = class {
       "p" : Piece.PAWN
     };
 
-    let board = fen.split(" ")[0];
+    const board = fen.split(" ")[0];
     let file = 0, rank = 0;
     for (let symbol in board) {
       symbol = board[symbol];
@@ -46,9 +95,11 @@ let Board = class {
         };
       };
     };
+
+    this.redrawCanvas();
   };
   importFromBase64(str) {
-    let binArray = atob(str);
+    const binArray = atob(str);
     for (let i = 0; i < binArray.length; i++) {
         let a = str.charCodeAt(i);
         for (let j = 0; j < 2; j++) {
@@ -56,10 +107,12 @@ let Board = class {
             a <<= 4;
         }
     };
+
+    this.redrawCanvas();
   };
   toString() {
-    let pieceTable = " 1234567.KQRNBP..kqrnbp.";
-    let rowMargin = "+---+---+---+---+---+---+---+---+\n";
+    const pieceTable = " 1234567.KQRNBP..kqrnbp.";
+    const rowMargin = "+---+---+---+---+---+---+---+---+\n";
     let output = rowMargin;
 
     for (let i = 0; i < 8; i++) {
@@ -74,7 +127,7 @@ let Board = class {
     return output + "  a   b   c   d   e   f   g   h  ";
   };
   getFENString() {
-    let pieceTable = " 1234567.KQRNBP..kqrnbp.";
+    const pieceTable = " 1234567.KQRNBP..kqrnbp.";
     let output = "";
     let emptySquareCounter = 0;
 
@@ -113,9 +166,26 @@ let Board = class {
   getPieceBitboard(type, isWhite) {
     
   };
-  drawBoard(boardEl) {
-    let labels = " KQRNBPX";
-    let boardRows = boardEl.children;
+  drawBoard(boardEl, sprites = ChessSprites) {
+    const labels = [" ", "wK", "wQ", "wR", "wN", "wB", "wP", " ", " ", "bK", "bQ", "bR", "bN", "bB", "bP", " "];
+    const boardRows = boardEl.children;
+    let boardGrids, pieceNum;
+
+    for (let i = 0; i < 8; i++) {
+      boardGrids = boardRows[i].children;
+      for (let j = 0; j < 8; j++) {
+        boardGrids[j].innerHTML = "";
+        pieceNum = this.squares[(i << 3) + j] - 8;
+        let sprite = sprites[labels[pieceNum]];
+        if (sprite) {
+          boardGrids[j].appendChild(sprite.cloneNode(true));
+        };
+      };
+    };
+  };
+  drawTextualBoard(boardEl) {
+    const labels = " KQRNBPX";
+    const boardRows = boardEl.children;
     let boardGrids, pieceNum, pieceType, pieceColor;
 
     for (let i = 0; i < 8; i++) {
@@ -228,12 +298,12 @@ let Move = class {
     return a.moveValue == b.moveValue;
   };
   toString() {
-    let promotionPiece = ".kqrnbp.";
+    const promotionPiece = ".kqrnbp.";
     return sqNumberToAlgebraic(this.moveValue >> 6) + sqNumberToAlgebraic(this.moveValue & 63) + promotionPiece[this.getPromotionPieceType()];
   };
   toBase64String() {
-    // let base64char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    let base64char = "456789+/wxyz0123opqrstuvghijklmnYZabcdefQRSTUVWXIJKLMNOPABCDEFGH";
+    // const base64char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    const base64char = "456789+/wxyz0123opqrstuvghijklmnYZabcdefQRSTUVWXIJKLMNOPABCDEFGH";
     return base64char[this.getStartSq()] + base64char[this.getTargetSq()] + this.getPromotionPieceType();
   };
 };
